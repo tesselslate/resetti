32efa33d4820f168ba33afa8e554f652c5583c60
woofdoggo <woofwoofdoggo@protonmail.com>
2022-04-20 16:36:42 -0400

    x11: Add Keymod type

--- x11.go
+++ x11.go
@@ -14,9 +14,23 @@ type Attributes struct {
 	class []string
 }
 
+type Keymod uint16
+
+const (
+	ModShift Keymod = 1 << 0
+	ModLock         = 1 << 1
+	ModCtrl         = 1 << 2
+	Mod1            = 1 << 3
+	Mod2            = 1 << 4
+	Mod3            = 1 << 5
+	Mod4            = 1 << 6
+	Mod5            = 1 << 7
+	ModNone         = 0
+)
+
 type Key struct {
 	code xproto.Keycode
-	mod  uint16
+	mod  Keymod
 }
 
 type XClient struct {
@@ -149,11 +163,11 @@ func (c *XClient) GetWindowList(win xproto.Window) ([]xproto.Window, error) {
 }
 
 func (c *XClient) GrabKey(key Key) {
-	xproto.GrabKey(c.x, true, c.root, key.mod, key.code, xproto.GrabModeAsync, xproto.GrabModeAsync)
+	xproto.GrabKey(c.x, true, c.root, uint16(key.mod), key.code, xproto.GrabModeAsync, xproto.GrabModeAsync)
 	c.keys = append(c.keys, key)
 }
 
-func (c *XClient) SendKey(press bool, win xproto.Window, key xproto.Keycode, mod uint16, timestamp xproto.Timestamp) error {
+func (c *XClient) SendKey(press bool, win xproto.Window, key xproto.Keycode, mod Keymod, timestamp xproto.Timestamp) error {
 	evt := xproto.KeyPressEvent{
 		Sequence:   0,
 		Detail:     key,
@@ -165,22 +179,22 @@ func (c *XClient) SendKey(press bool, win xproto.Window, key xproto.Keycode, mod
 		RootY:      0,
 		EventX:     0,
 		EventY:     0,
-		State:      mod,
+		State:      uint16(mod),
 		SameScreen: true,
 	}
 
 	if press {
-		reply := xproto.SendEventChecked(c.x, true, win, xproto.EventMaskKeyPress, evt.String())
+		reply := xproto.SendEventChecked(c.x, true, win, xproto.EventMaskKeyPress, string(evt.Bytes()))
 		return reply.Check()
 	} else {
 		evt := xproto.KeyReleaseEvent(evt)
-		reply := xproto.SendEventChecked(c.x, true, win, xproto.EventMaskKeyRelease, evt.String())
+		reply := xproto.SendEventChecked(c.x, true, win, xproto.EventMaskKeyRelease, string(evt.Bytes()))
 		return reply.Check()
 	}
 }
 
 func (c *XClient) UngrabKey(key Key) {
-	xproto.UngrabKey(c.x, key.code, c.root, key.mod)
+	xproto.UngrabKey(c.x, key.code, c.root, uint16(key.mod))
 
 	i := 0
 	for _, v := range c.keys {
