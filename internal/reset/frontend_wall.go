cb7454e878d38dc92c49c6f99aab08dd1e8fe4df
woofdoggo <woofwoofdoggo@protonmail.com>
2023-03-12 17:42:16 -0400

    reset: Ghost pie fix on wall

--- internal/reset/frontend_wall.go
+++ internal/reset/frontend_wall.go
@@ -57,11 +57,12 @@ func (f *FrontendWall) HandleInput(event x11.Event) error {
 					f.wallReset(id)
 				}
 			} else {
+				f.instances[f.active].PressF3(f.x.GetCurrentTime())
 				if err := f.instances[f.active].Stretch(f.conf); err != nil {
 					return err
 				}
 				go runHook(f.conf.Hooks.Reset)
-				f.host.ResetInstance(f.active, f.x.GetCurrentTime())
+				f.host.ResetInstance(f.active, f.x.GetCurrentTime()+5)
 				time.Sleep(time.Millisecond * time.Duration(f.conf.Reset.Delay))
 				if f.conf.Wall.GoToLocked {
 					for idx, state := range f.states {
