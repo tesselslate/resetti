27de4940bbf2f8042aa0971f937b20cc0d329b1a
woofdoggo <woofwoofdoggo@protonmail.com>
2023-04-06 12:23:46 -0400

    cfg: Add new default config

--- internal/cfg/cfg.go
+++ internal/cfg/cfg.go
@@ -1,7 +1,17 @@
 // Package cfg allows for reading the user's configuration.
 package cfg
 
-import "github.com/woofdoggo/resetti/internal/x11"
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+	"github.com/woofdoggo/resetti/internal/x11"
+)
+
+// go:embed default.toml
+var defaultProfile []byte
 
 // Hooks contains various commands to run whenever the user performs certain
 // actions.
@@ -18,7 +28,7 @@ type Keybinds struct {
 	Focus           x11.Key    `toml:"focus"`             // Focus instance/projector
 	Reset           x11.Key    `toml:"reset"`             // Reset all / reset current
 	WallLock        x11.Keymod `toml:"wall_lock"`         // (Un)lock instance
-	WallPlay        x11.Keymod `toml:"wall_play"`         // Play insatnce
+	WallPlay        x11.Keymod `toml:"wall_play"`         // Play instance
 	WallReset       x11.Keymod `toml:"wall_reset"`        // Reset from wall
 	WallResetOthers x11.Keymod `toml:"wall_reset_others"` // Focus reset from wall
 }
@@ -36,8 +46,8 @@ type Wall struct {
 	GotoLocked  bool `toml:"goto_locked"`  // Also known as wall bypass
 	GracePeriod int  `toml:"grace_period"` // Milliseconds to wait after preview before a reset can occur
 
-	StretchRes   *Rectangle `toml:"stretch_res"`   // Inactive resolution
-	UnstretchRes *Rectangle `toml:"unstretch_res"` // Active resolution
+	StretchRes   *Rectangle `toml:"stretch_res"` // Inactive resolution
+	UnstretchRes *Rectangle `toml:"play_res"`    // Active resolution
 	UseF1        bool       `toml:"use_f1"`
 
 	// Instance hiding (dirt cover) settings.
@@ -64,20 +74,20 @@ type Wall struct {
 		// each CCX.)
 		CcxSplit bool `toml:"ccx_split"`
 
-		CpusIdle   uint `toml:"affinity_idle"`   // CPUs for idle group
-		CpusLow    uint `toml:"affinity_low"`    // CPUs for low group
-		CpusMid    uint `toml:"affinity_mid"`    // CPUs for mid group
-		CpusHigh   uint `toml:"affinity_high"`   // CPUs for high group
-		CpusActive uint `toml:"affinity_active"` // CPUs for active group
+		CpusIdle   int `toml:"affinity_idle"`   // CPUs for idle group
+		CpusLow    int `toml:"affinity_low"`    // CPUs for low group
+		CpusMid    int `toml:"affinity_mid"`    // CPUs for mid group
+		CpusHigh   int `toml:"affinity_high"`   // CPUs for high group
+		CpusActive int `toml:"affinity_active"` // CPUs for active group
 
 		// The number of milliseconds to wait after an instance finishes
 		// generating to move it from the mid group to the idle group.
 		// A value of 0 disables this functionality.
-		BurstLength uint `toml:"burst_length"`
+		BurstLength int `toml:"burst_length"`
 
 		// The world generation percentage at which instances are moved from
 		// the high group to the low group.
-		LowThreshold uint `toml:"low_threshold"`
+		LowThreshold int `toml:"low_threshold"`
 	} `toml:"performance"`
 }
 
@@ -96,4 +106,82 @@ type Profile struct {
 // Rectangle is a rectangle. That's it.
 type Rectangle struct {
 	X, Y, W, H uint32
+}
+
+// GetDirectory returns the path to the user's configuration directory.
+func GetDirectory() (string, error) {
+	// UserConfigDir automatically checks for $XDG_CONFIG_HOME and falls back
+	// to $HOME/.config, so we don't need to do any special checks ourselves.
+	xdgDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	return xdgDir + "/resetti", nil
+}
+
+// GetProfile returns a parsed configuration profile.
+func GetProfile(name string) (Profile, error) {
+	dir, err := GetDirectory()
+	if err != nil {
+		return Profile{}, fmt.Errorf("get config directory: %w", err)
+	}
+	file, err := os.ReadFile(dir + name + ".toml")
+	if err != nil {
+		return Profile{}, fmt.Errorf("read config file: %w", err)
+	}
+	profile := Profile{}
+	if err = toml.Unmarshal(file, &profile); err != nil {
+		return Profile{}, fmt.Errorf("parse config file: %w", err)
+	}
+	if err = validateProfile(&profile); err != nil {
+		return Profile{}, fmt.Errorf("validate config: %w", err)
+	}
+	return profile, nil
+}
+
+// MakeProfile makes a new configuration profile with the given name and the
+// default settings.
+func MakeProfile(name string) error {
+	dir, err := GetDirectory()
+	if err != nil {
+		return fmt.Errorf("get config directory: %w", err)
+	}
+	if stat, err := os.Stat(dir); err != nil {
+		if os.IsNotExist(err) {
+			err := os.Mkdir(dir, 0644)
+			if err != nil {
+				return fmt.Errorf("create config directory: %w", err)
+			}
+		}
+		if !stat.IsDir() {
+			return fmt.Errorf("config directory (%s) is not a directory", dir)
+		}
+	}
+	return os.WriteFile(
+		dir+name+".toml",
+		[]byte(defaultProfile),
+		0644,
+	)
+}
+
+// validateProfile ensures that the user's configuration profile does not have
+// any illegal or invalid settings.
+func validateProfile(conf *Profile) error {
+	// TODO
+	return nil
+}
+
+func (r *Rectangle) UnmarshalTOML(value any) error {
+	str, ok := value.(string)
+	if !ok {
+		return errors.New("rectangle value was not a string")
+	}
+	n, err := fmt.Sscanf(str, "%dx%d+%d,%d", &r.W, &r.H, &r.X, &r.Y)
+	if err != nil {
+		return err
+	}
+	if n != 4 {
+		return errors.New("missing rectangle dimensions")
+	}
+	return nil
 }
