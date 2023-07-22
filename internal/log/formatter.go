96b3f4d2fba1782c4ed5ada7d175747fff9bc69f
sathya-pramodh <sathyapramodh17@gmail.com>
2023-07-22 21:50:03 +0530

    log: Added log colors.

--- internal/log/formatter.go
+++ internal/log/formatter.go
@@ -6,6 +6,16 @@ import (
 	"time"
 )
 
+// Color constants for logging.
+const (
+	Reset  string = "\033[0m"
+	White  string = "\033[97m"
+	Red    string = "\033[31m"
+	Cyan   string = "\033[36m"
+	Blue   string = "\033[34m"
+	Yellow string = "\033[33m"
+)
+
 // GetValues makes an array of strings containing all the format fields with their respective values.
 // Used internally to get the recognized format variables in `formatStr`.
 func GetValues(ascTime string, level string, message string, formatStr string) (map[string]string, error) {
@@ -33,6 +43,18 @@ func Format(level string, message string, formatStr string) (string, error) {
 	}
 	for field, value := range values {
 		formatStr = strings.ReplaceAll(formatStr, field, value)
+	}
+	switch level {
+	case "ERROR":
+		formatStr = Red + formatStr + Reset
+	case "WARN":
+		formatStr = Yellow + formatStr + Reset
+	case "INFO":
+		formatStr = White + formatStr + Reset
+	case "DEBUG":
+		formatStr = Cyan + formatStr + Reset
+	case "VERBOSE":
+		formatStr = Blue + formatStr + Reset
 	}
 	return formatStr + "\n", nil
 }
